5a6661305a13ef33f9e2a3de5aebe30e21260e5d
zhangxiao774 <zhangxiao15@jd.com>
2020-11-25 18:46:01 +0800

    feat:new

---
+++ test/source-data/source 2.js
@@ -0,0 +1,352 @@
+module.exports = {
+    "code": "0",
+    "message": "响应成功",
+    "result": {
+        "trumpList": [
+            {
+                "id": 6196,
+                "skuId": "100008603015",
+                "activeName": "OPPO Reno4 SE 新品今日开售",
+                "skuName": "OPPO Reno4 SE【现货开售】 8+128 超闪蓝 65W超级闪充 3200万前置自拍 轻薄外观 双模5G 拍照手机",
+                "skuIndexImg": "https://m.360buyimg.com/mobilecms/jfs/t1/151865/30/431/102453/5f681c09Ef003ae35/4890e8f36a55ebfb.jpg",
+                "lowSkuIndexImg": "",
+                "abTest": "B",
+                "jdPrice": "2499",
+                "saleType": 3,
+                "btnShow": "立即抢购",
+                "activityTips": [
+                    "12期免息送移动电源"
+                ],
+                "startTime": "09.25首发",
+                "day": "25",
+                "month": "09",
+                "description": "12期免息送移动电源",
+                "tagText": "",
+                "faceGifImg": "https://m.360buyimg.com/mobilecms/s915x500_jfs/t1/116940/8/18691/115286/5f6c62e0E10df842f/7679413f98f8655f.jpg!q70.jpg",
+                "lastModifyTime": 1601001299000,
+                "publishDate": 1600963200000,
+                "name": "OPPO Reno4 SE【现货开售】 8+128 超闪蓝 65W超级闪充 3200万前置自拍 轻薄外观 双模5G 拍照手机",
+                "iconImg": "https://m.360buyimg.com/img/s77x77_jfs/t1/101309/10/6025/19839/5df0c7d3Eec9bf2b7/80870c60a6a392db.png!q50",
+                "cardType": 1,
+                "skuImg": "https://m.360buyimg.com/img/s750x850_jfs/t1/116940/8/18691/115286/5f6c62e0E10df842f/7679413f98f8655f.jpg!q50",
+                "newSkuImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/136663/5/10583/114944/5f6c62e5E98741d26/f76fe8a8f0afd7a9.jpg",
+                "linkUrl": "https://pro.m.jd.com/mall/active/2GCeJktamTARLx9CdM87xTcCvh62/index.html",
+                "linkUrlPC": "https://pro.jd.com/mall/active/2GCeJktamTARLx9CdM87xTcCvh62/index.html",
+                "clkImpr": "0_6196_100008603015_OPPO Reno4 SE 新品今日开售_0_none_1_2",
+                "lottieShow": 1
+            },
+            {
+                "id": 6154,
+                "skuId": "100008483105",
+                "activeName": "三星Z Fold2 5G新品首销",
+                "skuName": "三星Galaxy Z Fold2 5G(SM-F9160)折叠屏 双模5G手机 骁龙865+ 内外双屏 120Hz自适应屏幕 12GB+512GB迷雾金",
+                "skuIndexImg": "https://m.360buyimg.com/mobilecms/jfs/t1/154702/14/659/47413/5f6c53daE7fbe15e7/758c14cc5e3b2be3.jpg",
+                "lowSkuIndexImg": "https://m.360buyimg.com/img/s266x242_jfs/t1/129536/28/12792/85063/5f61bb61E8616d290/04716a686fda2a0f.jpg!q5",
+                "abTest": "B",
+                "jdPrice": "16999",
+                "saleType": 3,
+                "btnShow": "立即抢购",
+                "activityTips": [
+                    "屏幕内外切换  操纵不间断"
+                ],
+                "startTime": "09.25首发",
+                "day": "25",
+                "month": "09",
+                "description": "屏幕内外切换  操纵不间断",
+                "tagText": "",
+                "faceClearImg": "",
+                "faceGifImg": "https://m.360buyimg.com/mobilecms/s849x837_jfs/t1/129536/28/12792/85063/5f61bb61E8616d290/04716a686fda2a0f.jpg!q70.jpg",
+                "lastModifyTime": 1600416684000,
+                "publishDate": 1600963200000,
+                "name": "三星Galaxy Z Fold2 5G(SM-F9160)折叠屏 双模5G手机 骁龙865+ 内外双屏 120Hz自适应屏幕 12GB+512GB迷雾金",
+                "iconText": "",
+                "iconImg": "https://m.360buyimg.com/img/s77x77_jfs/t1/86995/17/4739/19298/5de8e067Ea5493973/e7b4d5fb496b5f41.png!q50",
+                "cardType": 0,
+                "newSkuImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/145119/29/8666/50894/5f646baaE78c4648f/b59400a977c2eb03.jpg",
+                "linkUrl": "https://pro.m.jd.com/mall/active/rYVEW3uHoZ6MPVd2BnbBP6YBa1Y/index.html",
+                "linkUrlPC": "https://pro.jd.com/mall/active/rYVEW3uHoZ6MPVd2BnbBP6YBa1Y/index.html",
+                "clkImpr": "1_6154_100008483105_三星Z Fold2 5G新品首销_0_none_0_2"
+            },
+            {
+                "id": 6192,
+                "skuId": "100015593286",
+                "activeName": "戴尔G5新酷睿i7游戏本",
+                "skuName": "戴尔(DELL)G5 英特尔酷睿i7电竞游戏本笔记本电脑(十代标压i7 16G 512G固态硬盘 GTX1650Ti 4G 独显 )",
+                "skuIndexImg": "https://m.360buyimg.com/mobilecms/jfs/t1/141664/5/8783/128768/5f633586E31f434c4/2e8faf73c4694929.jpg",
+                "lowSkuIndexImg": "https://m.360buyimg.com/img/s266x242_jfs/t1/118104/26/18640/121733/5f6c3747Ea5546a32/6c6e9328a7bc1ba2.jpg!q5",
+                "abTest": "B",
+                "jdPrice": "6699",
+                "saleType": 3,
+                "btnShow": "立即抢购",
+                "activityTips": [
+                    "晒单返100元E卡"
+                ],
+                "startTime": "09.25首发",
+                "day": "25",
+                "month": "09",
+                "description": "晒单返100元E卡",
+                "tagText": "",
+                "faceClearImg": "",
+                "faceGifImg": "https://m.360buyimg.com/mobilecms/s849x837_jfs/t1/118104/26/18640/121733/5f6c3747Ea5546a32/6c6e9328a7bc1ba2.jpg!q70.jpg",
+                "lastModifyTime": 1601017450000,
+                "publishDate": 1600963200000,
+                "name": "戴尔(DELL)G5 英特尔酷睿i7电竞游戏本笔记本电脑(十代标压i7 16G 512G固态硬盘 GTX1650Ti 4G 独显 )",
+                "iconText": "",
+                "iconImg": "https://m.360buyimg.com/img/s77x77_jfs/t1/86995/17/4739/19298/5de8e067Ea5493973/e7b4d5fb496b5f41.png!q50",
+                "cardType": 0,
+                "newSkuImg": "https://m.360buyimg.com/img/jfs/t1/113080/30/18327/303740/5f6ab765Ead5df99b/7694cbdda109d449.jpg",
+                "linkUrl": "https://pro.m.jd.com/mall/active/39as17Pyeyqtf6w8fEPMjSZAxUqT/index.html",
+                "linkUrlPC": "https://pro.jd.com/mall/active/39as17Pyeyqtf6w8fEPMjSZAxUqT/index.html",
+                "clkImpr": "2_6192_100015593286_戴尔G5新酷睿i7游戏本_0_none_0_2"
+            }
+        ],
+        "newsTabList": [
+            {
+                "code": "1",
+                "name": "精选",
+                "proNum": 0
+            },
+            {
+                "code": "2",
+                "name": "潮科技",
+                "proNum": 0
+            }
+        ],
+        "shareInfo": {
+            "shareUrl": "https://news.m.jd.com?transparentenable=true",
+            "iconUrl": "https://img30.360buyimg.com/njmobilecms/jfs/t19108/138/2498155869/14489/88f5d493/5afaa100N965fd8d7.png",
+            "title": "总是期待已久，还好如期而至。科技、生活、时尚最新动态，邀您共赏。",
+            "content": "京东新品首发，全方位新品解读，最前沿新品资讯，热门新品0元试新。"
+        },
+        "bottomTabs": [
+            {
+                "name": "上新",
+                "tabType": 0,
+                "tabUrl": "https://news.m.jd.com",
+                "tabIcon": "https://img30.360buyimg.com/njmobilecms/jfs/t23431/228/637934281/5240/65ca20f9/5b3a04c1Ndb982d82.png",
+                "tabIconSelected": "https://img30.360buyimg.com/njmobilecms/jfs/t23194/23/641569670/4186/7b9649d5/5b3a059bN2f134484.png"
+            },
+            {
+                "name": "资讯",
+                "tabType": 1,
+                "tabUrl": "https://pro.m.jd.com/mall/active/328Q8WyZ6futcokJLaSdRdtNTZk/index.html",
+                "tabIcon": "https://img30.360buyimg.com/njmobilecms/jfs/t23635/100/644547032/4955/b484196e/5b3a05d0N4801d762.png",
+                "tabIconSelected": "https://img30.360buyimg.com/njmobilecms/jfs/t23758/95/634951670/4741/9e893883/5b3a05deN740ba955.png"
+            },
+            {
+                "name": "试用",
+                "tabType": 2,
+                "tabUrl": "https://pro.m.jd.com/mall/active/e8aNFCxEEsHDt8aShKNcJw5oUWi/index.html",
+                "tabIcon": "https://img30.360buyimg.com/njmobilecms/jfs/t21193/230/1855506958/4078/c6ac711/5b3a05adNf25eb968.png",
+                "tabIconSelected": "https://img30.360buyimg.com/njmobilecms/jfs/t22249/261/1850475474/4648/41b675c0/5b3a05c1N51eecc1c.png"
+            }
+        ],
+        "bottomTabsNew": [
+            {
+                "id": 2,
+                "name": "人气新品",
+                "tabType": 2,
+                "tabUrl": "",
+                "tabIcon": "https://m.360buyimg.com/njmobilecms/jfs/t1/43114/37/10152/6058/5d39ae4cEf10b5475/798d80a40c73968d.png",
+                "tabIconSelected": "https://m.360buyimg.com/njmobilecms/jfs/t1/45936/35/5926/9349/5d39ae52Eda530067/e072bf005d70569a.png",
+                "jump_key": "home",
+                "jump_param": "",
+                "redPointFlag": 0,
+                "redPointTime": 1559807304000,
+                "shareUrl": "https://news.m.jd.com?transparentenable=true&from=NewProductDebut_Share&utm_source=iosapp",
+                "shareIconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/69633/34/14214/5650/5db7e613E447ecdf6/202afa52840dc48b.jpg",
+                "shareTitle": "京东小魔方新品首发",
+                "shareContent": "上京东小魔方，发现更多超级新品~"
+            },
+            {
+                "id": 3,
+                "name": "新品资讯",
+                "tabType": 3,
+                "tabUrl": "",
+                "tabIcon": "https://m.360buyimg.com/njmobilecms/jfs/t1/98973/34/1195/8903/5dba8e5eEcc9a136f/b840da581bc104f5.png",
+                "tabIconSelected": "https://m.360buyimg.com/njmobilecms/jfs/t1/105662/18/1094/13542/5dba8e5fEa472cd19/cc687366ad7eec82.png",
+                "jump_key": "info",
+                "jump_param": "",
+                "redPointFlag": 0,
+                "redPointTime": 1550563179000,
+                "shareUrl": "https://news.m.jd.com/info?transparentenable=true&from=NewProductDebut_Share&to=info",
+                "shareIconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/95392/6/996/5650/5db7e9f5E7c2d7496/01fb5f5a36f9078d.jpg",
+                "shareTitle": "京东小魔方-新品资讯",
+                "shareContent": "上京东小魔方，与达人一起探秘新品的新潮趣玩~"
+            }
+        ],
+        "bottomTabsFree": [
+            {
+                "id": 2,
+                "name": "人气新品",
+                "tabType": 0,
+                "tabUrl": "",
+                "tabIcon": "https://m.360buyimg.com/njmobilecms/jfs/t1/43114/37/10152/6058/5d39ae4cEf10b5475/798d80a40c73968d.png",
+                "tabIconSelected": "https://m.360buyimg.com/njmobilecms/jfs/t1/45936/35/5926/9349/5d39ae52Eda530067/e072bf005d70569a.png",
+                "jump_key": "home",
+                "jump_param": "",
+                "jumpKey": "home",
+                "jumpParam": "",
+                "redPointFlag": 0,
+                "redPointTime": 1559807304000,
+                "shareUrl": "https://news.m.jd.com?transparentenable=true&from=NewProductDebut_Share&utm_source=iosapp",
+                "shareIconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/69633/34/14214/5650/5db7e613E447ecdf6/202afa52840dc48b.jpg",
+                "shareTitle": "京东小魔方新品首发",
+                "shareContent": "上京东小魔方，发现更多超级新品~"
+            },
+            {
+                "id": 8,
+                "name": "品牌上新",
+                "tabType": 3,
+                "tabUrl": "",
+                "tabIcon": "https://m.360buyimg.com/njmobilecms/jfs/t1/61294/29/5451/6601/5d39ae60Efc7ff938/36f74bec524dcd57.png",
+                "tabIconSelected": "https://m.360buyimg.com/njmobilecms/jfs/t1/48711/5/5933/9373/5d39ae69Efd1aef00/1ee98346fb386ac8.png",
+                "jump_key": "brand",
+                "jump_param": "",
+                "jumpKey": "brand",
+                "jumpParam": "",
+                "redPointFlag": 0,
+                "redPointTime": 1552233600000,
+                "shareUrl": "https://news.m.jd.com/brand?transparentenable=true&from=NewProductDebut_Share&to=brand",
+                "shareIconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/69941/14/14100/5650/5db7e976Ee4ae7f70/bd0e1850c6e4dad1.jpg",
+                "shareTitle": "京东小魔方-品牌上新",
+                "shareContent": "上京东小魔方，发现更多新潮大牌"
+            },
+            {
+                "id": 3,
+                "name": "新品资讯",
+                "tabType": 1,
+                "tabUrl": "",
+                "tabIcon": "https://m.360buyimg.com/njmobilecms/jfs/t1/98973/34/1195/8903/5dba8e5eEcc9a136f/b840da581bc104f5.png",
+                "tabIconSelected": "https://m.360buyimg.com/njmobilecms/jfs/t1/105662/18/1094/13542/5dba8e5fEa472cd19/cc687366ad7eec82.png",
+                "jump_key": "info",
+                "jump_param": "",
+                "jumpKey": "info",
+                "jumpParam": "",
+                "redPointFlag": 0,
+                "redPointTime": 1550563179000,
+                "shareUrl": "https://news.m.jd.com/info?transparentenable=true&from=NewProductDebut_Share&to=info",
+                "shareIconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/95392/6/996/5650/5db7e9f5E7c2d7496/01fb5f5a36f9078d.jpg",
+                "shareTitle": "京东小魔方-新品资讯",
+                "shareContent": "上京东小魔方，与达人一起探秘新品的新潮趣玩~"
+            }
+        ],
+        "bannerAutoTime": 5,
+        "topMsgs": [],
+        "earlyPlay": {
+            "items": []
+        },
+        "iconInfo": {
+            "id": 131,
+            "type": 1,
+            "name": " 浪琴表新品发布",
+            "iconUrl": "https://m.360buyimg.com/njmobilecms/jfs/t1/147677/8/9213/29786/5f6c775dEf3f362fd/bd682aff937bf99e.gif",
+            "jump": {
+                "des": "m",
+                "params": {
+                    "url": "https://pro.m.jd.com/mall/active/2zbiWZMiDQea6M4pf4LciEWVNFg/index.html"
+                }
+            },
+            "linkUrl": "https://pro.m.jd.com/mall/active/2zbiWZMiDQea6M4pf4LciEWVNFg/index.html",
+            "envId": ""
+        },
+        "newXViewInfoVo": {
+            "url": "",
+            "xviewType": 0,
+            "xviewNum": 0
+        },
+        "externalConfig": {
+            "isLottieShow": 0
+        },
+        "tabList": [
+            {
+                "id": 1,
+                "tabName": "精选",
+                "nomalIcon": "https://m.360buyimg.com/njmobilecms/jfs/t27436/244/973703463/1372/a376a45c/5bbdc5c0N17dcdc59.png",
+                "selectedIcon": "https://m.360buyimg.com/njmobilecms/jfs/t25357/260/1876906761/1492/2b7faad8/5bbdc5b9N5ee7c0ca.png",
+                "redPointFlag": 0,
+                "redPointTime": 1548671064000,
+                "atmosphereIcon": "",
+                "firstTabName": "精选",
+                "did": "19140400",
+                "tabType": 1,
+                "type": 1,
+                "tabDisplay": "精选",
+                "cid": "670,,#9987,,",
+                "biTestId": "1",
+                "biClk": "5##",
+                "mcInfo": "04256012-11513575-6801333714-N#0--120--1027--#1-0-#11-20165172#newChannelHd"
+            },
+            {
+                "id": 9,
+                "tabName": "手机",
+                "nomalIcon": "https://m.360buyimg.com/njmobilecms/jfs/t26785/244/1022924101/584/2d492bee/5bbf1b3fN2091292d.png",
+                "selectedIcon": "https://m.360buyimg.com/njmobilecms/jfs/t27796/243/1048525860/626/364d35d3/5bbf1b40N18d5d013.png",
+                "redPointFlag": 0,
+                "atmosphereIcon": "",
+                "firstTabName": "手机",
+                "did": "23211384",
+                "tabType": 1,
+                "type": 1,
+                "tabDisplay": "手机",
+                "cid": "9987,653,#9987,830,#9987,12854,",
+                "biTestId": "1",
+                "biClk": "5##",
+                "mcInfo": "04256012-11513575-6801325903-N#0--120--1027--#1-0-#11-20165172#newChannelHd"
+            },
+            {
+                "id": 4,
+                "tabName": "电器",
+                "nomalIcon": "https://m.360buyimg.com/njmobilecms/jfs/t25498/300/1932585011/595/78a99b6f/5bbf137dN33eb27ab.png",
+                "selectedIcon": "https://m.360buyimg.com/njmobilecms/jfs/t25699/349/1933617131/628/1092cd5a/5bbf137eNb9b6e9fa.png",
+                "redPointFlag": 0,
+                "redPointTime": 1549881365000,
+                "atmosphereIcon": "",
+                "firstTabName": "电器",
+                "did": "18897416",
+                "tabType": 1,
+                "type": 1,
+                "tabDisplay": "家电",
+                "cid": "737,,",
+                "biTestId": "1",
+                "brokerInfo": "eyJwIjoiNjIxMDM0IiwidGFnIjoiMjMiLCJjc191c2VyIjoiMiIsInBhbGFudGlyX2V4cGlkcyI6IlJ8TUlYVEFHX1JSfCIsImV4cGlkIjoiMCIsInJlcXNpZyI6ImNiOTI1YTM2YjkxMzcwOTNlZmI4ZjMzNmFmMzM2MjJkY2EyMDdiMjkifQ==",
+                "biClk": "1#cb925a36b9137093efb8f336af33622dca207b29-0-621034#20165172",
+                "mcInfo": "04256012-11513575-6801330229-N#0--120--1027--#1-0-#11-20165172#newChannelHd"
+            }
+        ],
+        "homeConfig": {
+            "id": 24,
+            "config": [
+                {
+                    "id": 139,
+                    "configType": 2,
+                    "title": "新品日历",
+                    "subTitle": "新品抢先看",
+                    "backImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/120054/24/9048/10222/5f2d81b1E7dac2090/6e51a6603ff130de.png",
+                    "iconImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/124655/40/8827/13234/5f2d81bdE28e35358/c7ffabffdeceb778.png",
+                    "linkUrl": ""
+                },
+                {
+                    "id": 149,
+                    "configType": 1,
+                    "title": "女排助力新品",
+                    "subTitle": "享专属福利",
+                    "backImg": "",
+                    "iconImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/132959/14/10829/20540/5f6c56f6Ee53904c3/d257f0198a9041fd.png",
+                    "linkUrl": "https://pro.m.jd.com/mall/active/2UM9YR7Epd3tD7fBVnu884fzqSYK/index.html"
+                },
+                {
+                    "id": 138,
+                    "configType": 1,
+                    "title": "新品直播",
+                    "subTitle": "上新了京东",
+                    "backImg": "",
+                    "iconImg": "https://m.360buyimg.com/njmobilecms/jfs/t1/128675/28/9000/5566/5f2d8243E365efda5/7493b91933023b77.png",
+                    "linkUrl": "https://pro.m.jd.com/mall/active/46hpDAGQywsPJ95LZEy8pWGtN5Ei/index.html"
+                }
+            ]
+        },
+        "trumpLottieShow": 1
+    },
+    "success": false
+}
